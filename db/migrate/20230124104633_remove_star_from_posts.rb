d03098162bf364d50f8360f683debf5e88386344
EC2 Default User <ec2-user@ip-172-31-21-198.ap-northeast-1.compute.internal>
2023-01-26 12:10:10 +0000

    migrate削除

--- db/migrate/20230124104633_remove_star_from_posts.rb
+++
@@ -1,5 +0,0 @@
-class RemoveStarFromPosts < ActiveRecord::Migration[6.1]
-  def change
-    remove_column :posts, :star, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_24_104925) do
+ActiveRecord::Schema.define(version: 2023_01_26_120655) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -50,9 +50,6 @@ ActiveRecord::Schema.define(version: 2023_01_24_104925) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
-  end
-
-  create_table "areas", force: :cascade do |t|
   end
 
   create_table "bookmarks", force: :cascade do |t|
